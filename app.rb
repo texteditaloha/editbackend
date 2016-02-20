c5f1c37d25a048191648d549024e17023c0dffdd
Midhun Krishna <kr1shn4.m1dhun@gmail.com>
2016-02-20 14:36:36 +0530

    test commit api

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
+require 'json'
+
 configure { set :server, :puma }
 
 class Post < ActiveRecord::Base
 end
 
 get '/' do
-  "Hello, world"
+  "Cate store"
+end
+
+get '/:key' do
+  "Cate store #{params[:key]}"
+end
+
+post '/set' do
+  content_type :json
+  if(params[:token] == ENV['TOKEN'])
+    status 402
+  else
+    Post.create!(key: params[:key], value: params[:value])
+    params[:key]
+  end
 end
